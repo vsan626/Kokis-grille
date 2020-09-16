75cfeee9089c7e3500556eb5012cd05fa47662ea
Danny (vai) San <vai.quoc.san@gmail.com>
2020-09-15 18:18:22 -0700

    pulling data from express server working and shows on postman. Need to clean up code and figure out settings for env file

--- src/components/HomePageComponents/Reviews/YelpReviews.js
+++ src/components/HomePageComponents/Reviews/YelpReviews.js
@@ -1,27 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 axios.defaults.baseURL = 'https://api.yelp.com';
+axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
 
 export default function YelpReviews() {
   const [reviews, setReviews] = useState({});
-
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect( () => {
-    async function fetchYelpData () {
-    try {
-      const reviewData = await axios.get(
-        'https://api.yelp.com/v3/businesses/kokis-teppanyaki-grille-tustin/reviews'
-      );
-      setReviews(reviewData.data)
-    } catch (error) {
-      console.error('error with axios get', error);
+  useEffect(() => {
+    async function fetchYelpData() {
+      try {
+        const reviewData = await axios.get(
+          `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/kokis-teppanyaki-grille-tustin/reviews`,
+          {
+            headers: {
+              Authorization: `Bearer ${process.env.YELP_API}`
+            }
+          }
+        );
+        setReviews(reviewData.data);
+      } catch (error) {
+        console.error('error with axios get', error);
+      }
     }
-  }
-  fetchYelpData();
+    fetchYelpData();
   });
   return (
     <div className="single-review">
-    {console.log('reviews state', reviews)}
+      {console.log('reviews state', reviews)}
       <img src="img/user.png" alt="" />
       <h4>Hulda Sutton</h4>
       <div className="star">
