21e78ef2c64d91389ae3613f3ee268c41830eb3e
Danny (vai) San <vai.quoc.san@gmail.com>
2020-09-16 21:20:58 -0700

    working on rendering reviews pulled from yelp api. Running into error of failed to execute insertBefore on Node

--- src/components/HomePageComponents/Reviews/YelpReviews.js
+++ src/components/HomePageComponents/Reviews/YelpReviews.js
@@ -1,47 +1,120 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-axios.defaults.baseURL = 'https://api.yelp.com';
-axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-
+const yelpApiKey = process.env.REACT_APP_YELP_API;
 
 export default function YelpReviews() {
-  const [reviews, setReviews] = useState({});
+  const [reviews, setReviews] = useState([]);
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() => {
-    async function fetchYelpData() {
-      try {
-        const reviewData = await axios.get(
-          `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/kokis-teppanyaki-grille-tustin/reviews`,
-          {
-            headers: {
-              Authorization: `Bearer ${process.env.YELP_API}`
-            }
+
+  const fetchYelpData = async () => {
+    try {
+      const reviewData = await axios.get(
+        `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/kokis-teppanyaki-grille-tustin/reviews`,
+        {
+          headers: {
+            Authorization: `Bearer ${yelpApiKey}`
           }
-        );
-        setReviews(reviewData.data);
-      } catch (error) {
-        console.error('error with axios get', error);
-      }
+        }
+      );
+      setReviews(reviewData.data.reviews);
+    } catch (error) {
+      console.error('error with axios get', error);
     }
+  };
+
+  useEffect(() => {
+    // async function fetchYelpData () {
+    //   try {
+    //     const reviewData = await axios.get(
+    //       `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/kokis-teppanyaki-grille-tustin/reviews`,
+    //       {
+    //         headers: {
+    //           Authorization: `Bearer ${yelpApiKey}`
+    //         }
+    //       }
+    //     );
+    //     setReviews(reviewData.data.reviews);
+    //   } catch (error) {
+    //     console.error('error with axios get', error);
+    //   }
+    // }
     fetchYelpData();
-  });
-  return (
-    <div className="single-review">
-      {console.log('reviews state', reviews)}
-      <img src="img/user.png" alt="" />
-      <h4>Hulda Sutton</h4>
-      <div className="star">
-        <span className="fa fa-star checked"></span>
-        <span className="fa fa-star checked"></span>
-        <span className="fa fa-star checked"></span>
-        <span className="fa fa-star checked"></span>
-        <span className="fa fa-star"></span>
-      </div>
-      <p>
-        “Accessories Here you can find the best computer accessory for your
-        laptop, monitor, printer, scanner, speaker. Here you can find the best
-        computer accessory for your laptop, monitor, printer, scanner, speaker.”
-      </p>
-    </div>
-  );
+  }, []);
+
+  const reviewsRating = (num) => {
+    //#region star render based on yelp api rating
+    if (num === 5) {
+      return (
+        <div className="star">
+          <span className="fa fa-star checked"></span>
+          <span className="fa fa-star checked"></span>
+          <span className="fa fa-star checked"></span>
+          <span className="fa fa-star checked"></span>
+          <span className="fa fa-star checked"></span>
+        </div>
+      );
+    } else if (num === 4) {
+      return (
+        <div className="star">
+          <span className="fa fa-star checked"></span>
+          <span className="fa fa-star checked"></span>
+          <span className="fa fa-star checked"></span>
+          <span className="fa fa-star checked"></span>
+          <span className="fa fa-star"></span>
+        </div>
+      );
+    } else if (num === 3) {
+      return (
+        <div className="star">
+          <span className="fa fa-star checked"></span>
+          <span className="fa fa-star checked"></span>
+          <span className="fa fa-star checked"></span>
+          <span className="fa fa-star"></span>
+          <span className="fa fa-star"></span>
+        </div>
+      );
+    } else if (num === 2) {
+      return (
+        <div className="star">
+          <span className="fa fa-star checked"></span>
+          <span className="fa fa-star checked"></span>
+          <span className="fa fa-star"></span>
+          <span className="fa fa-star"></span>
+          <span className="fa fa-star"></span>
+        </div>
+      );
+    } else if (num === 1) {
+      return (
+        <div className="star">
+          <span className="fa fa-star checked"></span>
+          <span className="fa fa-star"></span>
+          <span className="fa fa-star"></span>
+          <span className="fa fa-star"></span>
+          <span className="fa fa-star"></span>
+        </div>
+      );
+    } else {
+      return null;
+    }
+    //#endregion
+  };
+
+  if (reviews.length > 0) {
+    return (
+      <>
+        {reviews.map((review, key) => {
+          return (
+            <div key={key} className="single-review">
+              <img src={review.user.image_url} alt="" />
+              <h4>{review.user.name}</h4>
+              {reviewsRating(review.rating)}
+              <p>{review.text}</p>
+            </div>
+          );
+        })}
+      </>
+    );
+  } else {
+    return null;
+  }
 }
