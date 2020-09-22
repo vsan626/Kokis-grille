c0303d1e061013de5287d869f8461ec90fb44a31
Danny (vai) San <vai.quoc.san@gmail.com>
2020-09-22 15:11:13 -0700

    deployed on netlify

---
+++ build/precache-manifest.11ba14468005d3b21aabaa30c6dfb76f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95b073e1a9b28c28ee44bab73b129af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7baa938e36f3ffe1fe05",
+    "url": "/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "66668c1987a56061a9a4",
+    "url": "/static/js/2.1e9b8050.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1e9b8050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7baa938e36f3ffe1fe05",
+    "url": "/static/js/main.0c11d33f.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
